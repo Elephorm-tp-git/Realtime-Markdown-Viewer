b9f352e0b21dd9172c8c28c768d9de089bbf7c17
sabativi <victor.sabatier@gmail.com>
2015-07-21 17:08:30 +0200

    add code parser

--- converter/markdown.js
+++ converter/markdown.js
@@ -8,6 +8,15 @@
    return str;
  }
 
+ var parseCode = function(str) {
+  var codeRegExp = /```(.*?)```/;
+  var stra = [];
+  while ((stra = codeRegExp.exec(str)) !== null) {
+    str = str.replace(stra[0], '<code>' + stra[1] + '</code>');
+  }
+  return str;
+ }
+
  var parseHorizontaleLine = function(str) {
   var horizontalRegExp = /^(?:([\*\-_] ?)+)\1\1$/gm;
   var stra = [];
@@ -21,6 +30,7 @@ var markdown = {
   parse: function (str, strict) {
     'use strict';
     str = parseHeadline(str);
+    str = parseCode(str);
     str = parseHorizontaleLine(str);
     return str;
   }
