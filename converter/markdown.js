18f763cd175b31d1b4ad1ba5a143f770a754574d
victor <victor.sabatier@gmail.com>
2015-07-31 10:23:11 +0200

    Revert "add image parser"
    This reverts commit 7b843356be2d5b1a175a85f8b1d69b308c1e0080.

--- converter/markdown.js
+++ converter/markdown.js
@@ -58,11 +58,7 @@ var parseStrong = function(str) {
   var linkRegExp = /\[([^\[]+)\]\(([^\)]+)\)/;
   var stra = [];
   while ((stra = linkRegExp.exec(str)) !== null) {
-    if (stra[0].substr(0, 1) === '!') {
-        str = str.replace(stra[0], '<img src="' + stra[2] + '" alt="' + stra[1] + '" title="' + stra[1] + '" />\n');
-    } else {
-        str = str.replace(stra[0], '<a ' + 'href="' + stra[2] + '">' + stra[1] + '</a>');
-    }
+    str = str.replace(stra[0], '<a ' + 'href="' + stra[2] + '">' + stra[1] + '</a>');
   }
   return str;
  }
